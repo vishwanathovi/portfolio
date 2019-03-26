b391c77ec351e4fc48ddf30df7de0314e35443ce
Vishwa <vishwanathovi@gmail.com>
2019-03-26 11:14:24 +0530

    Updates

---
+++ component---src-pages-404-tsx-1abb5ec189b1482a7c26.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{218:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t(72),r=t.n(l),c=t(71),o=t(210),u=t.n(o),i=function(e){var n=e.children,t=e.container?a.createElement(c.a,null,n):n;return a.createElement("section",{className:u.a.segment},t)};i.defaultProps={container:!0};n.default=function(){return a.createElement(a.Fragment,null,a.createElement(r.a,{title:"404 Not Found"}),a.createElement(i,null,a.createElement("h1",null,"Page was not found")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1abb5ec189b1482a7c26.js.map
